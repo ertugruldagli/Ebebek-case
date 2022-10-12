56da5af70cd64574c6c328ea8e89b83ad5175593
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-12 14:48:09 +0300

    commit

--- FirstCaseMaas/src/MaasHesaplayici/Employee.java
+++ FirstCaseMaas/src/MaasHesaplayici/Employee.java
@@ -13,4 +13,31 @@ public class Employee {
         this.workHours = workHours;
         this.hireYear = hireYear;
     }
+
+     public void tax(int salary){
+
+        if (salary>1000){
+            double vergi=salary*0.03;
+            double maas=salary-vergi;
+        }
+
+    }
+
+     public void bonus(int workHours){
+        if(workHours>40){
+
+        }
+    }
+
+    public void raiseSalary(){
+
+    }
+
+     public void tostring(){
+         System.out.println("Calisanin adi: " + this.name);
+         System.out.println("Calisanin maasi: " + this.salary);
+         System.out.println("Calisanin haftalik calisma saati: " + this.workHours);
+         System.out.println("Calisanin ise baslangic yili: " + this.hireYear);
+
+    }
 }
