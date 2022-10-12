1d2e91ba52dcec0715e46969008280496ffd20d6
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-12 17:04:40 +0300

    commit

--- FirstCaseMaas/src/MaasHesaplayici/Employee.java
+++ FirstCaseMaas/src/MaasHesaplayici/Employee.java
@@ -15,5 +15,57 @@ public class Employee {
         this.workHours = workHours;
         this.hireYear = hireYear;
     }
+        //VERGİ HESAPLAMA
+    public double tax(){
+        if(this.salary<1000){
+            return this.salary;
+        }else{
+            return this.salary*0.03;
+        }
+
+
+    }
+
+    //BONUS HESAPLAMA
+    public double bonus() {
+        if (this.workHours > 40) {
+            return (this.workHours - 40) * 30;
+        }else{
+            return 0;
+        }
+    }
+
+    //ZAM HESAPLAMA
+    public double raiseSalary(){
+        int year=2021-this.hireYear;
+
+        if(year>19) {
+            return this.salary*0.15;
+        }else if(year>9 && year <20){
+            return this.salary*0.10;
+        }
+        else {
+            return this.salary*0.05;
+        }
+    }
+
+
+
+    public void tostring(){
+        System.out.println("ad: " + this.name);
+        System.out.println("Maas: " + this.salary);
+        System.out.println("Haftalik Saat: " + this.workHours);
+        System.out.println("Giris yili: " + this.hireYear);
+        System.out.println("--------------------");
+        System.out.println("Vergi: " + tax());
+        System.out.println("bonus: " + bonus());
+        System.out.println("Maas Artisi: " + raiseSalary());
+        System.out.println("--------------------");
+        System.out.println("vergi+bonus+maas: " +  (this.salary - tax() +bonus()));
+        System.out.println("toplam maas: " +  (this.salary - tax() +bonus()+ raiseSalary()));
+
+
+
+    }
 }
 
--- FirstCaseMaas/src/MaasHesaplayici/Main.java
+++ FirstCaseMaas/src/MaasHesaplayici/Main.java
@@ -2,8 +2,8 @@ package MaasHesaplayici;
 
 public class Main {
     public static void main(String[] args) {
-        Employee e1=new Employee("Ertu", 5000, 40, 1989);
-
+        Employee e1=new Employee("Ertu", 2000, 45, 1985);
+        e1.tostring();
     }
 
 
