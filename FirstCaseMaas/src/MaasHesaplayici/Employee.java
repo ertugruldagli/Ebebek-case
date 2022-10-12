89643cdad094b9dcfe05e189ee21ba57746b8b9e
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-12 14:35:11 +0300

    constructor commit

--- FirstCaseMaas/src/MaasHesaplayici/Employee.java
+++ FirstCaseMaas/src/MaasHesaplayici/Employee.java
@@ -1,4 +1,16 @@
 package MaasHesaplayici;
 
 public class Employee {
+
+    String name;
+    int salary;
+    int workHours;
+    int hireYear;
+
+    public Employee(String name, int salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
 }
