58e2178a1c70956343d7be50637bb9be265c3854
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-12 15:48:27 +0300

    commit

--- FirstCaseMaas/src/MaasHesaplayici/Employee.java
+++ FirstCaseMaas/src/MaasHesaplayici/Employee.java
@@ -3,11 +3,11 @@ package MaasHesaplayici;
 public class Employee {
 
     String name;
-    int salary;
+    double salary;
     int workHours;
     int hireYear;
-    double vergi;
-    double maas;
+
+
 
     public Employee(String name, int salary, int workHours, int hireYear) {
         this.name = name;
@@ -15,53 +15,5 @@ public class Employee {
         this.workHours = workHours;
         this.hireYear = hireYear;
     }
-
-     public void tax(){
-        tostring();
-        if (this.salary>1000) {
-            vergi = this.salary * 0.03;
-            maas = this.salary - vergi;
-            System.out.println("vergili maasi: " + maas);
-        }
-    }
-
-
-
-     public void bonus(){
-        if(this.workHours>40){
-                while(true){
-                    this.salary+=30;
-                }
-        }
-    }
-
-
-
-    public void raiseSalary(){
-        double year=2021-this.hireYear;
-        double yeni=0.0;
-
-        if (year<9){
-            yeni=this.salary*0.05;
-            yeni+=this.salary;
-            System.out.println("zamli maasi: " + yeni);
-        } else if (year>=9 && year<20) {
-            yeni=this.salary*0.10;
-            yeni+=this.salary;
-            System.out.println("zamli maasi: " + yeni);
-        }else{
-            yeni=this.salary*0.15;
-            yeni+=this.salary;
-            System.out.println("zamli maasi: " + yeni);
-        }
-
-    }
-
-     public void tostring(){
-         System.out.println("Calisanin adi: " + this.name);
-         System.out.println("Calisanin maasi: " + this.salary);
-         System.out.println("Calisanin haftalik calisma saati: " + this.workHours);
-         System.out.println("Calisanin ise baslangic yili: " + this.hireYear);
-
-    }
 }
+
--- FirstCaseMaas/src/MaasHesaplayici/Main.java
+++ FirstCaseMaas/src/MaasHesaplayici/Main.java
@@ -3,7 +3,7 @@ package MaasHesaplayici;
 public class Main {
     public static void main(String[] args) {
         Employee e1=new Employee("Ertu", 5000, 40, 1989);
-        e1.raiseSalary();
+
     }
 
 
