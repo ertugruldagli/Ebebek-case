8783275af911d1ae5f0eb02bf23126c96e6e8d4c
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-19 10:23:59 +0300

    commit

--- FirstCaseMaas/src/MaasHesaplayici/Main.java
+++ FirstCaseMaas/src/MaasHesaplayici/Main.java
@@ -1,10 +1,18 @@
 package MaasHesaplayici;
-
 public class Main {
-    public static void main(String[] args) {
-        Employee e1=new Employee("Ertugrul DAGLI", 2000, 45, 1985);
-        e1.tostring();
+        public static void main(String[] args) {
+            // Employee e1=new Employee("Ertugrul DAGLI", 2000, 45, 1985);
+            // e1.tostring();
+
+
+
+
     }
 
 
 }
+
+
+
+
+
